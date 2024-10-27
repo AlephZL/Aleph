b4b3438fe58ecd0b894f3e5598d6b728a5574090
AlephZL <alvinfebrian1140@gmail.com>
2024-10-27 21:31:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const meteorid = document.getElementById('meteorid');
 const rocket = document.getElementById('rocket');
 const text = document.getElementById('text');
 const button = document.getElementById('button');
+const audio = document.getElementById('audio');
+const playPauseButton = document.getElementById('play-pause');
+
+audio.volume = 0.75;
+
+playPauseButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.textContent = '⏸'; // Pause icon
+        playPauseButton.classList.remove('play-btn');
+        playPauseButton.classList.add('pause-btn');
+    } else {
+        audio.pause();
+        playPauseButton.textContent = '▶'; // Play icon
+        playPauseButton.classList.remove('pause-btn');
+        playPauseButton.classList.add('play-btn');
+    }
+});
+
 
 window.addEventListener('scroll', ()=> {
     const value = window.scrollY;
